b57ea10496ff2873796637afa7c46cb742f42209
Matthew Berginski <matthew.berginski@unc.edu>
2018-02-06 16:37:12 -0500

    adding kinase list CSV and reading/parsing

--- lib/DarkKinaseKB.pm
+++ lib/DarkKinaseKB.pm
@@ -1,5 +1,8 @@
 package DarkKinaseKB;
 use Dancer2;
+use Text::CSV::Simple;
+use Cwd;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
@@ -8,10 +11,20 @@ get '/' => sub {
 };
 
 get '/kinase/:kinase' => sub {
+  my $parser = Text::CSV::Simple->new;
+  my @kinase_info = $parser->read_file('../data_sets/full_kinase_list.csv') or die "$!";
+  
+  my @kinase_list;
+  for (0..(scalar(@kinase_info) - 1)) {
+    push @kinase_list, $kinase_info[$_][0];
+  }
+
+  my $kinase = params->{kinase};
   
 
-  template 'kinase' => { 'kinase' => params->{kinase} };
-};
+  my %template_data = ('kinase' => params->{kinase});
 
+  template 'kinase' => \%template_data;
+};
 
 true;
