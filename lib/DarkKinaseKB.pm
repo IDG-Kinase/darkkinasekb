887e9b738c97e08ed3142a3c6e31fab8369beb4d
Matthew Berginski <matthew.berginski@unc.edu>
2018-02-07 13:57:45 -0500

    updates to front page with list of kinases

--- lib/DarkKinaseKB.pm
+++ lib/DarkKinaseKB.pm
@@ -3,11 +3,36 @@ use Dancer2;
 use Text::CSV::Simple;
 use Cwd;
 use Data::Dumper;
+use Math::Round;
+use POSIX;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'DarkKinaseKB' };
+  my $parser = Text::CSV::Simple->new;
+  my @kinase_info = $parser->read_file('../data_sets/full_kinase_list.csv') or die "$!";
+  
+  my @kinase_list;
+  #skip the first line, header
+  for (1..(scalar(@kinase_info) - 1)) {
+    push @kinase_list, $kinase_info[$_][0];
+  }
+
+  @kinase_list = sort @kinase_list;
+  
+  my $kinase_list_length = ceil(scalar(@kinase_list)/3);
+    
+  my %split_kinase_lists = (
+    1 => [ splice @kinase_list, 0, $kinase_list_length ],
+    2 => [ splice @kinase_list, 0, $kinase_list_length ],
+    3 => [ splice @kinase_list, 0 ]
+  );
+
+  template 'index' => { 'title' => 'DarkKinaseKB' , 
+                        'kinase_1' => $split_kinase_lists{1},
+                        'kinase_2' => $split_kinase_lists{2},
+                        'kinase_3' => $split_kinase_lists{3}
+                      };
 };
 
 get '/kinase/:kinase' => sub {
@@ -20,9 +45,8 @@ get '/kinase/:kinase' => sub {
   }
 
   my $kinase = params->{kinase};
-  
 
-  my %template_data = ('kinase' => params->{kinase});
+  my %template_data = ('kinase' => $kinase, 'title' => $kinase);
 
   template 'kinase' => \%template_data;
 };
