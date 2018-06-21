6d129630c676e3c52a7b212020c31bacce298242
Matthew Berginski <matthew.berginski@unc.edu>
2018-06-21 15:38:12 -0400

    adding Monarch Initiative link to each kinase page

--- lib/DarkKinaseKB.pm
+++ lib/DarkKinaseKB.pm
@@ -41,14 +41,20 @@ get '/kinase/:kinase' => sub {
   my $parser = Text::CSV::Simple->new;
   my @kinase_info = $parser->read_file('../data_sets/full_kinase_list.csv') or die "$!";
   
-  my @kinase_list;
-  for (0..(scalar(@kinase_info) - 1)) {
-    push @kinase_list, $kinase_info[$_][0];
-  }
-
   my $kinase = params->{kinase};
+  
+  my @kinase_info = grep $_->[1] eq $kinase, @kinase_info;
 
-  my %template_data = ('kinase' => $kinase, 'title' => $kinase);
+  debug('test');
+  debug($kinase_info[0][0]);
+
+  my $hgnc_num = "NA"; 
+  if($kinase_info[0][0] =~ /(\d+)/) {
+    $hgnc_num = $1;
+  }
+  debug($hgnc_num);
+
+  my %template_data = ('kinase' => $kinase, 'title' => $kinase, 'hgnc_num' => $hgnc_num);
 
   template 'kinase' => \%template_data;
 };
