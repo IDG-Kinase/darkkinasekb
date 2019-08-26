c647eb7622fbead0758c3593bb723121cb6c7b58
Matthew Berginski <matthew.berginski@gmail.com>
2019-08-26 16:26:17 -0400

    updating NanoBRET tracer sheets

--- lib/NanoBRET.pm
+++ lib/NanoBRET.pm
@@ -27,7 +27,8 @@ get '/NanoBRET' => sub {
 	foreach my $index (0..$#{$template_data{NanoBRET_info}}) {
 		my $this_kinase = $template_data{NanoBRET_info}[$index]{symbol};
 		my @tracer_sheet_hits = grep $_ =~ /NL-$this_kinase(-Cyclin)? / | 
-									 $_ =~ /$this_kinase-NL(-Cyclin)? /, @tracer_sheets;
+									 $_ =~ /$this_kinase-NL(-Cyclin)? / |
+									 $_ =~ /$this_kinase /, @tracer_sheets;
 		if (scalar(@tracer_sheet_hits) > 0) {
 			$template_data{NanoBRET_info}[$index]{tracer_sheet_file} = basename($tracer_sheet_hits[0]);
 		} else {
