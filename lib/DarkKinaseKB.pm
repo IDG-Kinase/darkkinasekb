fd19d5cee50044a84a53bb55fbcfa7849193ca7d
Matthew Berginski <matthew.berginski@gmail.com>
2021-04-28 13:25:54 -0400

    fixing bug in searching for plasmids associated with each kinase

--- lib/DarkKinaseKB.pm
+++ lib/DarkKinaseKB.pm
@@ -253,7 +253,7 @@ get '/kinase/:kinase' => sub {
 	my @plasmid_hits = grep $_->{'symbol'} eq $template_data{kinase}, @{$plasmid_info};
 	
 	$template_data{include_plasmids} = 0;
-	if (scalar(@NanoBRET_hits) > 0) {
+	if (scalar(@plasmid_hits) > 0) {
 		$template_data{include_plasmids} = 1;
 		$template_data{plasmid_info} = \@plasmid_hits;
 	}
