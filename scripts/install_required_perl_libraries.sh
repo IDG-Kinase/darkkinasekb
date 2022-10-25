0555cbd0134885d92bff27839319a97fd70669ad
Matthew Berginski <matthew.berginski@gmail.com>
2022-10-25 19:23:15 -0400

    updating needed perl libraries

--- scripts/install_required_perl_libraries.sh
+++ scripts/install_required_perl_libraries.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-sudo cpanm Dancer YAML Template Config::General Archive::Extract Crypt::SaltedHash Dancer2 Text::CSV::Simple Math::Round Text::Fuzzy Text::CSV
+sudo cpanm Dancer YAML Template Config::General Archive::Extract Crypt::SaltedHash Dancer2 Text::CSV::Simple Math::Round Text::Fuzzy Text::CSV Plack::Runner
